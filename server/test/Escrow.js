24cc4810933833866901fd02360c6f6eeeba3d34
ANIL KUMAR BEHERA <ark845612@gmail.com>
2024-04-14 20:42:47 +0530

    test: deployment and owner update checked

--- server/test/Escrow.js
+++ server/test/Escrow.js
@@ -6,15 +6,66 @@ const token = (n) => {
 };
 
 describe("Escrow", () => {
-  it("saves the addresses", async () => {
+  let buyer, seller, inspector, lender;
+  let realEstate, escrow;
+
+  beforeEach(async () => {
+    //setup accounts
+    [buyer, seller, inspector, lender] = await ethers.getSigners();
+
     //Deploy real estate
     const RealEstate = await ethers.getContractFactory("RealEstate");
-    const realEstate = await RealEstate.deploy();
-    console.log(realEstate.address);
+    realEstate = await RealEstate.deploy();
 
     //Mint
-    let transaction = await realEstate.mint(
-      "https://ipfs.io/ipfs/QmTudSYeM7mz3PkYEWXWqPjomRPHogcMFSq7XAvsvsgAPS"
+    let transaction = await realEstate
+      .connect(seller)
+      .mint(
+        "https://ipfs.io/ipfs/QmTudSYeM7mz3PkYEWXWqPjomRPHogcMFSq7XAvsvsgAPS"
+      );
+    await transaction.wait();
+    // console.log(transaction);
+
+    const Escrow = await ethers.getContractFactory("Escrow");
+    escrow = await Escrow.deploy(
+      realEstate.target,
+      seller.address,
+      inspector.address,
+      lender.address
     );
+
+    // Approve property
+    transaction = await realEstate.connect(seller).approve(escrow.target, 0);
+    await transaction.wait();
+
+    //List property
+    transaction = await escrow.connect(seller).list(0);
+    await transaction.wait();
+  });
+
+  describe("Deployment", () => {
+    it("returns NFT address", async () => {
+      const result = await escrow.nftAddress();
+      expect(result).to.be.equal(realEstate.target);
+    });
+    it("returns seller", async () => {
+      const result = await escrow.seller();
+      expect(result).to.be.equal(seller.address);
+    });
+
+    it("returns lender", async () => {
+      const result = await escrow.lender();
+      expect(result).to.be.equal(lender.address);
+    });
+
+    it("returns inspector", async () => {
+      const result = await escrow.inspector();
+      expect(result).to.be.equal(inspector.address);
+    });
+  });
+  describe("Listing", () => {
+    it("Updates ownership", async () => {
+      expect(await realEstate.ownerOf(0)).to.be.equal(escrow.target);
+    });
   });
 });
