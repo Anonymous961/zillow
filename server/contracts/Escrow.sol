820df9f3cbb7ceb25da286947ff218629cf0471e
ANIL KUMAR BEHERA <ark845612@gmail.com>
2024-04-15 22:11:30 +0530

    contract: deposit of earnest and inspection

--- server/contracts/Escrow.sol
+++ server/contracts/Escrow.sol
@@ -15,10 +15,27 @@ contract Escrow {
     address public inspector;
     address public lender;
 
+    modifier onlyBuyer(uint256 _nftID){
+        require(msg.sender == buyer[_nftID],"Only buyer can call this method");
+        _;
+    }
+    
+    modifier onlySeller(){
+        require(msg.sender == seller,"Only seller can call this method");
+        _;
+    }
+
+    modifier onlyInspector(){
+        require(msg.sender == inspector,"Only inspector can call this method");
+        _;
+    }
+
+
     mapping(uint256=> bool) public isListed;
     mapping(uint256 => uint256) public purchasePrice;
     mapping(uint256 => uint256)public escrowAmount;
     mapping(uint256 => address)public buyer;
+    mapping(uint256 => bool) public inspectionPassed;
 
     constructor(
         address _nftAddress,
@@ -32,7 +49,7 @@ contract Escrow {
         lender = _lender;
     }
 
-    function list(uint256 _nftID,address _buyer, uint256 _purchasePrice, uint256 _escrowAmount) public{
+    function list(uint256 _nftID,address _buyer, uint256 _purchasePrice, uint256 _escrowAmount) public payable onlySeller{
         // Transfer NFT from seller to this contract
         IERC721(nftAddress).transferFrom(msg.sender, address(this),_nftID);
 
@@ -40,6 +57,21 @@ contract Escrow {
         purchasePrice[_nftID]=_purchasePrice;
         escrowAmount[_nftID]=_escrowAmount;
         buyer[_nftID]=_buyer;
+    }
 
+    // Put under Contract (only buyer - payable escrow)
+    function depositEarnest(uint _nftID) public payable onlyBuyer(_nftID){
+        require(msg.value >= escrowAmount[_nftID]);
+    }
+
+    // Update Inspection Status (only inspector)
+    function updateInspectionStatus(uint256 _nftID, bool _passed)public onlyInspector{
+        inspectionPassed[_nftID]=_passed;
+    }
+
+    receive() external payable{}
+
+    function getBalance() public view returns(uint256){
+        return address(this).balance;
     }
 }
